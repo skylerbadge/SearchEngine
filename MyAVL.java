cfe1cc0b99d35963f4f97d6aed0624cb13305ca7
Skyler <skyler.badge@gmail.com>
2018-10-19 22:26:44 +0530

    Myavl done

---
+++ MyAVL.java
@@ -0,0 +1,132 @@
+public class MyAVL<T extends Comparable<T>> {
+    class Node{
+        T data;
+        Node leftChild, rightChild, parent;
+        int height;
+        public Node()
+        {
+            data = null;
+            leftChild = null;
+            rightChild = null;
+            parent = null;
+            height = 0;
+            
+        }
+        public Node(T o){
+            data = o;
+            leftChild = null;
+            rightChild = null;
+            parent = null;
+            height = 0;
+        }
+    }
+    
+    Node root;
+    
+    public MyAVL(){
+        root = null;
+    }
+    public boolean isEmpty()
+    {
+         return root == null;
+    }
+    public int height(Node n){
+        if(n == null)
+            return -1;
+        else
+            return n.height;
+    }
+    public Node search(T o){
+        Node ptr = root;
+        while(ptr!=null){
+            if (ptr.data.compareTo(o)==0)
+                break;
+            else if(ptr.data.compareTo(o)>0)
+                ptr = ptr.rightChild;
+            else
+                ptr = ptr.leftChild;
+        }
+        return ptr;
+    }
+    
+    public Node rotateRight(Node n){
+       Node lch = n.leftChild;
+       Node T = lch.rightChild;
+        
+       lch.rightChild = n;
+       n.leftChild = T;
+       
+       n.height = Math.max(height(n.leftChild),height(n.rightChild))+1;
+       lch.height = Math.max(height(lch.leftChild),height(lch.rightChild))+1;
+
+       return lch;
+    }
+    public Node rotateLeft(Node n){
+       Node rch = n.rightChild;
+       Node T = rch.leftChild;
+        
+       rch.leftChild = n;
+       n.rightChild = T;
+
+       n.height = Math.max(height(n.leftChild),height(n.rightChild))+1;
+       rch.height = Math.max(height(rch.leftChild),height(rch.rightChild))+1;
+       
+       return rch;
+    }
+
+    public void insert(T o){
+        root = insertrec(o,root);
+    }
+    
+    public Node insertrec(T o, Node t)
+    {
+         if (t == null)
+             t = new Node(o);
+         else if (t.data.compareTo(o)<0)
+         {
+             t.leftChild = insertrec( o, t.leftChild );
+             if( height( t.leftChild ) - height( t.rightChild ) == 2 )
+                 if(t.leftChild.data.compareTo(o)<0)
+                     t = rotateRight( t );
+                 else{
+                     t.leftChild = rotateLeft(t.leftChild);
+                     t = rotateRight(t);
+                 }
+         }
+         else
+         {
+             t.rightChild = insertrec( o, t.rightChild );
+             if( height( t.rightChild ) - height( t.leftChild ) == 2 )
+                 if(t.rightChild.data.compareTo(o)>0)
+                     t = rotateLeft(t);
+                 else{
+                     t.rightChild = rotateRight(t.rightChild);
+                     t = rotateLeft(t);
+                 }
+         }
+         t.height = Math.max( height( t.leftChild ), height( t.rightChild ) ) + 1;
+         return t;
+    }
+    
+    public void inorder(Node r)
+     {
+         if (r != null)
+         {
+             inorder(r.leftChild);
+             System.out.print(r.data +" ");
+             inorder(r.rightChild);
+         }
+     }
+    public static void main(String[] args) {
+        MyAVL<Integer> av = new MyAVL<Integer>();
+        av.insert(5);
+        av.insert(45);
+        av.insert(30);
+        av.insert(3);
+        av.insert(0);
+        av.insert(50);
+        av.insert(2);
+        
+        av.inorder(av.root);
+    }
+}
--- Position.java
+++ Position.java
@@ -1,6 +1,7 @@
-public class Position{
+public class Position implements Comparable<Position>{
     PageEntry p;
     int wi;
+    int swi; // stored word index
 
     public Position(PageEntry p, int wordIndex){
         this.p = p;
@@ -11,5 +12,11 @@ public class Position{
     }
     public int getWordIndex(){
         return wi;
+    }
+    
+    @Override
+    public int compareTo(Position obj)
+    {
+        return obj.swi - this.swi;
     }
 }
